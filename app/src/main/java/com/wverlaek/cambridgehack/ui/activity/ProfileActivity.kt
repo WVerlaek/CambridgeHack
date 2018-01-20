15012c135d5f769707ecae10d4f17c8e982689e3
polyphemus <rolfmorel@gmail.com>
2018-01-20 20:51:18 +0000

    Show photo

--- app/src/main/java/com/wverlaek/cambridgehack/ui/activity/ProfileActivity.kt
+++ app/src/main/java/com/wverlaek/cambridgehack/ui/activity/ProfileActivity.kt
@@ -14,11 +14,23 @@ import com.wverlaek.cambridgehack.database.Repository
 import com.wverlaek.cambridgehack.database.models.Profile
 import kotlinx.android.synthetic.main.activity_profile.*
 import org.jetbrains.anko.toast
+import android.content.Intent
+import android.net.Uri
+import android.widget.ImageView
+import com.google.firebase.storage.FirebaseStorage
+import java.net.URI
+
 
 class ProfileActivity : AppCompatActivity() {
     val TAG = "PROFILE"
     val UID_TAG = "UID"
+    val PICK_IMAGE = 1
     var repo : Repository = Repository()
+    var storage = FirebaseStorage.getInstance()
+    var storageRef = storage.getReference()
+
+    private lateinit var uid: String
+
     private lateinit var nameText: TextView
     private lateinit var backgroundText: TextView
     private lateinit var btnSubmit: Button
@@ -28,7 +40,7 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         if (intent == null || !intent.hasExtra(UID_TAG)) return
-        val uid = intent.getStringExtra(UID_TAG)
+        uid = intent.getStringExtra(UID_TAG)
 
         nameText = findViewById(R.id.name_field)
         backgroundText = findViewById(R.id.background_field)
@@ -59,8 +71,38 @@ class ProfileActivity : AppCompatActivity() {
             toast("Updated your profile")
             finish()
         }
+
+        val photoSelect : Button = findViewById(R.id.btn_photo)
+
+        photoSelect.setOnClickListener {
+            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getIntent.type = "image/*"
+
+            val pickIntent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickIntent.type = "image/*"
+
+            val chooserIntent = Intent.createChooser(getIntent, "Select Image")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+            startActivityForResult(chooserIntent, PICK_IMAGE)
+        }
     }
 
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        Log.d(TAG, "onActivityResult")
+        if (requestCode == PICK_IMAGE) {
+            val uri : Uri = data.getData()
+            Log.d(TAG, "selected image: " + uri)
+            val ref = storageRef.child("images/" + uid)
+
+            ref.putFile(uri)
+
+            toast("Your picture is stored ")
+
+            val photo: ImageView = findViewById(R.id.profile_photo)
+            photo.setImageURI(uri)
+        }
+    }
 
 
 }
