19ad4a778363246588522ded5530b0c2fd99d0d9
Bas <basser94@gmail.com>
2018-01-21 04:27:53 +0000

    Small change

--- app/src/main/java/com/wverlaek/cambridgehack/ui/activity/MainActivity.kt
+++ app/src/main/java/com/wverlaek/cambridgehack/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.wverlaek.cambridgehack.R
@@ -53,6 +54,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == rcSignIn) {
+            val resp = IdpResponse.fromResultIntent(data)
             if (resultCode == Activity.RESULT_OK) {
                 onLoggedIn(auth.currentUser!!)
             } else {
