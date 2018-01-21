a9e06d76985f1ca0164629f087c88e39b82adc63
wverlaek <wverlaek@hotmail.nl>
2018-01-21 10:42:54 +0000

    Github api fix

--- app/src/main/java/com/wverlaek/cambridgehack/network/GithubService.kt
+++ app/src/main/java/com/wverlaek/cambridgehack/network/GithubService.kt
@@ -2,6 +2,7 @@ package com.wverlaek.cambridgehack.network
 
 import com.wverlaek.cambridgehack.database.models.github.Repo
 import com.wverlaek.cambridgehack.database.models.github.User
+import com.wverlaek.cambridgehack.util.Constants
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,10 +13,10 @@ import retrofit2.http.Path
  * Created by WVerl on 20-1-2018.
  */
 interface GithubService {
-    @GET("users/{user}")
+    @GET("users/{user}?client_id=73b2c4b86a769623ca66&client_secret=01cfde68999384ad3e5f4e1967bdb5094822a136")
     fun getUser(@Path("user") user: String): Call<User>
 
-    @GET("users/{user}/repos")
+    @GET("users/{user}/repos?client_id=73b2c4b86a769623ca66&client_secret=01cfde68999384ad3e5f4e1967bdb5094822a136")
     fun getRepos(@Path("user") user: String): Call<List<Repo>>
 
     companion object {
--- app/src/main/java/com/wverlaek/cambridgehack/util/Constants.kt
+++ app/src/main/java/com/wverlaek/cambridgehack/util/Constants.kt
@@ -10,4 +10,7 @@ object Constants {
     val MS_GROUP_ID = "c_h"
 
     val PROFILE_ICON_EXTRA_SIZE = 20
+
+    val GITHUB_ID = "73b2c4b86a769623ca66"
+    val GITHUB_SECRET = "01cfde68999384ad3e5f4e1967bdb5094822a136"
 }
